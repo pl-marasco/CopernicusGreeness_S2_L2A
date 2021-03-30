5e9e4d6671a3753a1c3beaef7b9c6622699ea3b5
Pier <phenoloboy@users.noreply.github.com>
2021-03-30 11:49:31 +0200

    numba injected

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ import yaml
 import matplotlib.pyplot as plt
 import datetime
 import glob
+from distributed import Client, progress
+from numba import njit
+
+
+def _decades(data):
+    if data[-1] == 1:
+        diff = np.diff(data)
+        # return np.split(data, np.where(np.diff(data) != 0)[0]+1)[-1].size
+        return np.split(data, np.where(np.logical_and(~np.isnan(diff), diff != 0))[0]+1)[-1].size
+    else:
+        return -999
 
 
 def _folder_walker(scene_path):
@@ -29,27 +40,33 @@ def _folder_walker(scene_path):
     datelist = pd.date_range(end=datetime.datetime.today(), periods=120, freq='D').tolist()
 
     for obs_date in datelist:
-        print(rf'Observation date:{obs_date.date()}')
         year = str(obs_date.year)
         month = str(obs_date.month).zfill(2)
         day = str(obs_date.day).zfill(2)
 
         path_date = os.path.join(path, year, month, day)
 
-        tile_date = []
+        # tile_date = []
         if os.path.isdir(path_date):
+            dic_zones = {zn: [] for zn in zones}
             for orbit in orbits:
 
                 path_orbit = os.path.join(path_date, orbit)
 
                 if os.path.isdir(path_orbit):
                     for folder in os.listdir(path_orbit):
-                        if folder[39:42] in zones:
+                        zone = folder[39:42]
+                        if zone in zones:
+
                             path_granule = os.path.join(path_orbit, folder, 'GRANULE')
                             for observation in os.listdir(path_granule):
                                 bands_path = os.path.join(path_granule, observation, 'IMG_data', 'R20m')
-                                tile_date.append(bands_path)
-            tiles.append(tile_date)
+                                upd_lst_int = dic_zones.get(zone)
+                                upd_lst_int.append(bands_path)
+                                dic_zones.update({zone: upd_lst_int})
+                                # tile_date.append(bands_path)
+            # tiles.append(tile_date)
+            tiles.append(list(filter(None, dic_zones.values())))
 
     return tiles
 
@@ -105,15 +122,48 @@ def _quality_mask(ds):
                          (ds.SCL != 9))).notnull()
 
 
+@njit(cache=True)
+def __ndvicpu(R, NIR, scale=10000.):
+    NDVI_ = np.zeros(R.shape, dtype=np.float64)
+    rows, cols = R.shape
+
+    for y in range(0, rows):
+        for x in range(0, cols):
+            R_ = R[y, x]/scale
+            NIR_ = NIR[y, x]/scale
+            denominator = (NIR_ + R_)
+
+            if not denominator == 0 or denominator == np.NAN:
+                ndvi_ = (NIR_ - R_) / (NIR_ + R_)
+                NDVI_[y, x] = ndvi_
+            else:
+                NDVI_[y, x] = np.NAN
+
+
+
+    return NDVI_
+
+
 def _ndvi(red, nir):
 
-    ndvi = (nir - red) / (nir + red)
-    ndvi.name = 'NDVI'
+    ndvi_arr = __ndvicpu(red.values[0], nir.values[0])
 
-    return ndvi
+    ndvi_arr = np.expand_dims(ndvi_arr, 0)
+
+    ndvi_da = xr.DataArray(ndvi_arr,
+                           dims=['time', 'y', 'x'],
+                           coords={'time': nir.time, 'y': nir.y, 'x': nir.x, 'spatial_ref': nir.spatial_ref},
+                           attrs={'scale_factor': nir.scale_factor,
+                                  'add_offset': nir.add_offset,
+                                  'grid_mapping': nir.grid_mapping})
+
+    ndvi_da.name = 'NDVI'
+
+    return ndvi_da
 
 
-def _evi(blue, red, nir):
+@njit(cache=True)
+def __evicpu(blue, red, nir):
     blue, red, nir = blue/1e4, red/1e4, nir/1e4
 
     evi = 2.5 * ((nir - red) / (nir + 6 * red - 7.5 * blue + 1))
@@ -123,26 +173,112 @@ def _evi(blue, red, nir):
     return evi
 
 
+def _evi(blue, red, nir):
+    evi_arr = __evicpu(blue.values[0], red.values[0], nir.values[0])
+
+    evi_arr = np.expand_dims(evi_arr, 0)
+
+    evi_da = xr.DataArray(evi_arr,
+                          dims=['time', 'y', 'x'],
+                          coords={'time': nir.time, 'y': nir.y, 'x': nir.x, 'spatial_ref': nir.spatial_ref},
+                          attrs={'scale_factor': nir.scale_factor,
+                                 'add_offset': nir.add_offset,
+                                 'grid_mapping': nir.grid_mapping})
+
+    evi_da.name = 'EVI'
+
+    return  evi_da
+
+
+@njit(cache=True)
+def _rgb2hsvcpu(R, G, B, scale=10000.):
+    """
+    RGB to HSV
+    Convert RGB values to HSV taking into account negative values
+    Adapted from https://stackoverflow.com/questions/39118528/rgb-to-hsl-conversion
+
+    :param
+    R_ : float
+      red channel
+    G_ : float
+      green channel
+    B_ : float
+      blue channel
+    scale: float, optional
+
+    :returns
+    H, S, V : float
+
+    """
+    H = np.zeros(R.shape, dtype=np.float64)
+    S = np.zeros(R.shape, dtype=np.float64)
+    V = np.zeros(R.shape, dtype=np.float64)
+
+    rows, cols = R.shape
+    for y in range(0, rows):
+        for x in range(0, cols):
+            h, s, v = 0, 0, 0
+
+            R_ = R[y, x]/scale
+            G_ = G[y, x]/scale
+            B_ = B[y, x]/scale
+
+            Cmax = max(R_, G_, B_)
+            Cmin = min(R_, G_, B_)
+            croma = Cmax - Cmin
+
+            if croma == 0:
+                H[y, x] = np.NAN
+                S[y, x] = np.NAN
+                V[y, x] = np.NAN
+                continue
+
+            if Cmax == R_:
+                segment = (G_ - B_ ) / croma
+                shift = 0 / 60
+                if segment < 0:
+                    shift = 360 / 60
+                h = segment + shift
+
+            if Cmax == G_:
+                 segment = (B_ - R_) / croma
+                 shift   = 120 / 60
+                 h = segment + shift
+
+            if Cmax == B_:
+                segment = (R_ - G_) / croma
+                shift   = 240 / 60
+                h = segment + shift
+
+            h *= 60.
+            v = Cmax
+            s = croma/v
+
+            H[y, x] = h
+            S[y, x] = s*100.
+            V[y, x] = v* 100.
+
+    return H, S, V
+
+
 def _hsv(mir, nir, red):
 
-    rgb = np.dstack((mir[0], nir[0], red[0]))
-    hsv = rgb2hsv(rgb)
+    h_arr, s_arr, v_arr = _rgb2hsvcpu(mir.values[0], nir.values[0], red.values[0])
+    h_arr = np.expand_dims(h_arr, 0)
+    s_arr = np.expand_dims(s_arr, 0)
+    v_arr = np.expand_dims(v_arr, 0)
 
-    H = np.expand_dims(hsv[:, :, 0], 0)
-    S = np.expand_dims(hsv[:, :, 1], 0)
-    V = np.expand_dims(hsv[:, :, 2], 0)
-
-    H = xr.DataArray(H,
+    H = xr.DataArray(h_arr,
                      dims=['time', 'y', 'x'],
                      coords={'time': mir.time, 'y': mir.y, 'x': mir.x})
 
     H = H.where(H != 0, np.NAN) * 360.
 
-    S = xr.DataArray(S,
+    S = xr.DataArray(s_arr,
                      dims=['time', 'y', 'x'],
                      coords={'time': mir.time, 'y': mir.y, 'x': mir.x})
 
-    V = xr.DataArray(V,
+    V = xr.DataArray(v_arr,
                      dims=['time', 'y', 'x'],
                      coords={'time': mir.time, 'y': mir.y, 'x': mir.x})
 
@@ -172,38 +308,64 @@ def _gvi(combined, **parameters):
 
 if __name__ == '__main__':
 
+    client = Client()
+
+    sensor = 'A'
+    decad = '20210221'
     alg = 'NDVI'
     parameters = 'parameters.yaml'
     limits = _threshold(parameters)
 
-    tiles_pth = _folder_walker(r'L:\HSL\observations\S2\scenes')
+    tiles_pth = _folder_walker(r'L:\HSL\observations\S2\sub')
 
-    dates = []
+    dates_da = []
 
-    for tiles_agg_date in tiles_pth:
-        tiles = []
-        for sng_date_tile in tiles_agg_date:
-            ds = _composer(sng_date_tile)
-            mask = _quality_mask(ds)
-            ds_masked = ds.where(mask)
+    for agg_date in tiles_pth:
+        for agg_utm in agg_date:
+            tiles = []
 
-            ndvi = _ndvi(ds_masked.B04, ds_masked.B8A)
-            evi = _evi(ds_masked.B02, ds_masked.B04, ds_masked.B8A)
-            hsv = _hsv(ds_masked.B11, ds_masked.B8A, ds_masked.B04)
+            for sng_obs_tile in agg_utm:
+                ds = _composer(sng_obs_tile)
+                mask = _quality_mask(ds)
+                ds_masked = ds.where(mask)
 
-            if alg == 'EVI':
-                combined = [evi, hsv.H]
-            elif alg == 'NDVI':
-                combined = [ndvi, hsv.H]
+                hsv = _hsv(ds_masked.B11, ds_masked.B8A, ds_masked.B04)
 
-            gvi_ = _gvi(combined, **{'limits': limits})
+                if alg == 'EVI':
+                    evi = _evi(ds_masked.B02, ds_masked.B04, ds_masked.B8A)
+                    combined = [evi, hsv.H]
+                elif alg == 'NDVI':
+                    ndvi = _ndvi(ds_masked.B04, ds_masked.B8A)
+                    combined = [ndvi, hsv.H]
 
-            gvi_reproj = gvi_.rio.reproject('EPSG:4326')
-            gvi_ready = gvi_reproj.where(gvi_reproj != -9999)
+                gvi_ = _gvi(combined, **{'limits': limits})
 
-            tiles.append(gvi_ready)
+                tiles.append(gvi_)
 
-        dates.append(merge_arrays(tiles, method='max'))
+            sng_date_utm_strip = merge_arrays(tiles, method='max', nodata=np.NAN)
 
-    test = xr.concat(dates, dim='time')
+            sng_date_reproj = sng_date_utm_strip.rio.reproject('EPSG:4326', )
+
+            dates_da.append(sng_date_reproj)
+
+    # time_agg = xr.combine_nested(dates_da, concat_dim=['time'])
+    time_agg = xr.combine_nested(dates_da, concat_dim=[['time', 'x', 'y']])
+
+    GVDM = xr.apply_ufunc(_decades, time_agg,
+                          input_core_dims=[['time']],
+                          exclude_dims={'time', },
+                          dask='parallelized',
+                          dask_gufunc_kwargs={'allow_rechunk': True},
+                          vectorize=True,)
+    GVDM.name = 'GVI'
+
+    GVDM_f = GVDM.to_netcdf(rf'L:\HSL\observations\S2\GVI_S2{sensor}_{decad}_{alg}.nc',
+                            compute=False,
+                            encoding={'GVI': {'_FillValue': -999}}).persist()
+
+    progress(GVDM_f)
+
+    client.close()
+
+
     pass
